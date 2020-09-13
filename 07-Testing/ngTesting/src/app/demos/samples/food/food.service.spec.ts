27e2f782280d998664417c393a507d1836163c48
Alexander Pajer <alexander.pajer@integrations.at>
2020-09-13 12:16:57 +0200

    food service setup

--- 07-Testing/ngTesting/src/app/demos/samples/food/food.service.spec.ts
+++ 07-Testing/ngTesting/src/app/demos/samples/food/food.service.spec.ts
@@ -1,11 +1,11 @@
-import { FoodService } from './food.service';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { FoodItem } from './food.model';
+import { FoodService } from './food.service';
 
 describe('Service - HttpTest -FoodService', () => {
   let service: FoodService;
@@ -23,18 +23,7 @@ describe('Service - HttpTest -FoodService', () => {
 
     service = TestBed.inject(FoodService);
     controller = TestBed.inject(HttpTestingController);
-  });
 
-  // Verify that there are no pending HTTP requests
-  afterEach(() => {
-    controller.verify();
-  });
-
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
-
-  it('should return initialized the data', (done) => {
     // setup the service mock
     const url = `${environment.api}food`;
     const req = controller.expectOne(url);
@@ -45,24 +34,37 @@ describe('Service - HttpTest -FoodService', () => {
 
     // make sure all requests are completed
     controller.verify();
-    // testing a private method - use any type to fool the compiler
+
+    // set the state
     const fs: any = service;
     fs.setState(data);
+  });
 
+  // Verify that there are no pending HTTP requests
+  afterEach(() => {
+    controller.verify();
+  });
+
+  it('should be created and correctly setup', () => {
+    expect(service).toBeTruthy();
+    // expect(service.getItems()).toBeObservable(of(data));
+  });
+
+  it('should return initialized the data', (done) => {
     service.getItems().subscribe((items) => {
       expect(items.length).toEqual(2);
       done();
     });
   });
 
-  // it('should create a post in an array', (done) => {
-  //   service.addItem({ name: 'Gulasch', rating: 2 });
+  it('should create a post in an array', (done) => {
+    service.addItem({ name: 'Gulasch', rating: 2 });
 
-  //   service.getItems().subscribe((items) => {
-  //     expect(items.length).toEqual(1);
-  //     done();
-  //   });
-  // });
+    service.getItems().subscribe((items) => {
+      expect(items.length).toEqual(3);
+      done();
+    });
+  });
 
   // it('should return the correct amount of items', (done) => {
   //   service.addItem({ name: 'Gulasch', rating: 2 });
