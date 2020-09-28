4bf9cbd48cf550658e7f83395c455e201d8b9c10
Alexander Pajer <alexander.pajer@integrations.at>
2020-09-28 21:32:21 +0200

    l3 starter

--- Lab/FoodL3/FoodL3Starter/src/app/shared/navbar/navbar.component.ts
+++ Lab/FoodL3/FoodL3Starter/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavItem } from './navItem';
+import { MenuService } from '../../menu/menu.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { NavItem } from './navItem';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private ms: MenuService) {}
 
   items: NavItem[];
 
@@ -17,5 +18,9 @@ export class NavbarComponent implements OnInit {
       { title: 'Products', url: '/products' },
       { title: 'About', url: '/about' },
     ];
+  }
+
+  toggleMenu() {
+    this.ms.toggleMenu();
   }
 }
