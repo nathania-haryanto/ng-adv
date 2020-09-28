300c5200999b96ab4be2e64236dd7e9151615d25
Alexander Pajer <alexander.pajer@integrations.at>
2020-09-28 23:13:04 +0200

    L4 scaffold

---
+++ Lab/FoodL4/FoodL4Solution/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  authEnabled: true,
+  api: 'http://localhost:3000/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDrWVNwZqyS2MZfn6R0jfKMai4BwIA7IEA',
+    authDomain: 'xxxx.firebaseapp.com',
+    databaseURL: 'https://xxxx.firebaseio.com',
+    projectId: 'xxxx',
+    storageBucket: 'xxxx.appspot.com',
+    messagingSenderId: '534119571593',
+    appId: '1:534119571593:web:1ed12d91e545e2ef8b6658',
+    measurementId: 'G-0JBHMCSF7H',
+  },
+};
---
+++ Lab/FoodL4/readme.md
@@ -0,0 +1,5 @@
+# Firebase Authentication using NgRx - Lab 4
+
+## Implement Firbase Auth using NgRx
+
+- Extend environment.ts to provide the Firebase Config
