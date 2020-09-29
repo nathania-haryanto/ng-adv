e83181a3a124e99611b867f63f06eea24e8454cb
Alexander Pajer <alexander.pajer@integrations.at>
2020-09-29 12:53:16 +0200

    code fix

--- 03-Reactive/ngReactive/src/app/demos/samples/custom-operators/filterEven.ts
+++ 03-Reactive/ngReactive/src/app/demos/samples/custom-operators/filterEven.ts
@@ -1,14 +1,14 @@
 import { Observable } from 'rxjs';
 
-export function filterEven(source: Observable<any>): Observable<any> {
-  return new Observable(observer => {
+export function filterEven(source: Observable<number>): Observable<number> {
+  return new Observable((observer) => {
     source.subscribe(
       (val: number) => {
         if (val % 2 === 0) {
           observer.next(val);
         }
       },
-      err => observer.error(err),
+      (err) => observer.error(err),
       () => observer.complete()
     );
   });
