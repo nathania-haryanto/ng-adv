49ae57e8440f9e818b91c1dc5093d3863618e266
alengauer <alengauer.training@gmail.com>
2020-10-26 08:22:20 +0100

    errors object based on form observables - simplified

--- 02-ReactiveForms/ngReactiveForms/src/app/demos/samples/reactive-validation/reactive-validation.component.ts
+++ 02-ReactiveForms/ngReactiveForms/src/app/demos/samples/reactive-validation/reactive-validation.component.ts
@@ -6,6 +6,8 @@ import {
   ValidationErrors,
   Validators,
 } from '@angular/forms';
+import { BehaviorSubject, combineLatest, merge } from 'rxjs';
+import { distinct, distinctUntilChanged, map, mergeAll, mergeMap, tap } from 'rxjs/operators';
 import { emptyPerson, wealthOpts } from '../empty-person';
 import { Person } from '../person.model';
 import { PersonService } from '../person.service';
@@ -17,7 +19,7 @@ import { AsyncMailExistsValidator } from './asyncMailExistsValidator';
   styleUrls: ['./reactive-validation.component.scss'],
 })
 export class ReactiveValidationComponent implements OnInit {
-  person: Person = emptyPerson;
+  person: Person = { ...emptyPerson, lastname:null};
   wealthOpts = wealthOpts;
 
   personForm: FormGroup;
@@ -46,6 +48,10 @@ export class ReactiveValidationComponent implements OnInit {
         this.person.name,
         [Validators.required, Validators.minLength(4), this.validateName],
       ],
+     lastname: [
+        this.person.lastname,
+        [Validators.required, this.validateLastName],
+      ],
       age: [this.person.age, [Validators.min(18), Validators.max(99)]],
       gender: [this.person.gender],
       email: [
@@ -56,22 +62,59 @@ export class ReactiveValidationComponent implements OnInit {
       ],
       wealth: [this.person.wealth],
     });
+    
   }
 
+  errors :any = {lastname:{}};
+
   private subscribeChanges() {
-    this.personForm.valueChanges.subscribe((vals) => {
-      console.log('changes happening @form: ', vals);
-    });
+    //this.personForm.valueChanges.subscribe((vals) => {
+    //  console.log('changes happening @form: ', vals);
+    //});
+
+    combineLatest(
+      [
+        this.personForm.valueChanges,
+        this.personForm.statusChanges.pipe(distinct())
+      ]
+    ).subscribe(el=>this.checkFormErrors(el))
+  }
+
+  private checkFormErrors(val_stat_changes:[{Person},string]) {
+    let state = val_stat_changes[1]
+    let person = val_stat_changes[0]
+    if(state === 'INVALID') {
+      let mod_fields = Object.keys(this.personForm.controls)
+      for(let el of mod_fields) {
+        let fp = this.personForm.get(el)
+        console.log(fp)
+        if(fp && fp.invalid && (fp.dirty || fp.touched)) {
+          this.errors[el] = fp.errors
+        }
+      }
+    } 
+    //this.errors=next_val
   }
 
   savePerson(personForm): void {
     this.ps.save(personForm);
   }
 
-  //Sample for custom Validator
+  //Sample for custom Validator - name
   validateName(control: FormControl): { [s: string]: boolean } {
     if (control.value === 'Hugo') {
       return { nameError: true };
+    }
+    return null;
+  }
+
+  //Sample for custom Validator - lastname
+  validateLastName(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Besenstiel') {
+      return { nameError: true };
+    }
+    if(control.value?.length<4 || control.value?.length>15) {
+      return { lengthError: true}
     }
     return null;
   }
