5faae8254b28ee4f4bf730ad9410c229d1010eaa
alengauer <alengauer.training@gmail.com>
2020-10-26 21:08:54 +0100

    observable errors

--- 02-ReactiveForms/ngReactiveForms/src/app/demos/samples/reactive-validation/reactive-validation.component.ts
+++ 02-ReactiveForms/ngReactiveForms/src/app/demos/samples/reactive-validation/reactive-validation.component.ts
@@ -6,7 +6,7 @@ import {
   ValidationErrors,
   Validators,
 } from '@angular/forms';
-import { BehaviorSubject, combineLatest, merge } from 'rxjs';
+import { BehaviorSubject, combineLatest, merge, Observable, Subject } from 'rxjs';
 import { distinct, distinctUntilChanged, map, mergeAll, mergeMap, tap } from 'rxjs/operators';
 import { emptyPerson, wealthOpts } from '../empty-person';
 import { Person } from '../person.model';
@@ -19,7 +19,7 @@ import { AsyncMailExistsValidator } from './asyncMailExistsValidator';
   styleUrls: ['./reactive-validation.component.scss'],
 })
 export class ReactiveValidationComponent implements OnInit {
-  person: Person = { ...emptyPerson, lastname:null};
+  person: Person = { ...emptyPerson, lastname: null };
   wealthOpts = wealthOpts;
 
   personForm: FormGroup;
@@ -28,7 +28,7 @@ export class ReactiveValidationComponent implements OnInit {
     private fb: FormBuilder,
     private ps: PersonService,
     private mailExistsValidator: AsyncMailExistsValidator
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.loadData();
@@ -48,7 +48,7 @@ export class ReactiveValidationComponent implements OnInit {
         this.person.name,
         [Validators.required, Validators.minLength(4), this.validateName],
       ],
-     lastname: [
+      lastname: [
         this.person.lastname,
         [Validators.required, this.validateLastName],
       ],
@@ -62,22 +62,27 @@ export class ReactiveValidationComponent implements OnInit {
       ],
       wealth: [this.person.wealth],
     });
-    
+
   }
 
+
+  // private subscribeChanges() {
+  //   this.personForm.valueChanges.subscribe((vals) => {
+  //     console.log('changes happening @form: ', vals);
+  //   });
+  // }
+
+  // <!--Form Error Object -->
+  /* 
   errors :any = {lastname:{}};
 
   private subscribeChanges() {
-    //this.personForm.valueChanges.subscribe((vals) => {
-    //  console.log('changes happening @form: ', vals);
-    //});
-
-    combineLatest(
-      [
-        this.personForm.valueChanges,
-        this.personForm.statusChanges.pipe(distinct())
-      ]
-    ).subscribe(el=>this.checkFormErrors(el))
+   combineLatest(
+     [
+       this.personForm.valueChanges,
+       this.personForm.statusChanges.pipe(distinct())
+     ]
+   ).subscribe(el=>this.checkFormErrors(el))  // unsubscribe missing!
   }
 
   private checkFormErrors(val_stat_changes:[{Person},string]) {
@@ -88,13 +93,59 @@ export class ReactiveValidationComponent implements OnInit {
       for(let el of mod_fields) {
         let fp = this.personForm.get(el)
         console.log(fp)
-        if(fp && fp.invalid && (fp.dirty || fp.touched)) {
+        if(fp && fp.invalid && (fp.dirty || fp.touched)) {  // touched is true after first blur
           this.errors[el] = fp.errors
         }
       }
     } 
-    //this.errors=next_val
   }
+  */
+  // END <!--Form Error Object -->
+
+
+  
+
+  // <!--Form Error Observable -->
+  errors$: Observable<any>; // = new Subject().asObservable();
+
+  private subscribeChanges() {
+    this.errors$ = combineLatest(
+      [
+        this.personForm.valueChanges,
+        this.personForm.statusChanges.pipe(distinct())
+      ]
+    ).pipe(
+      map(el => this.checkFormErrors(el))
+    )
+  }
+
+  private checkFormErrors(val_stat_changes: [{ Person }, string]) {
+    let state = val_stat_changes[1]
+    let person = val_stat_changes[0]
+    let errors: any = { lastname: {} };
+    if (state === 'INVALID') {
+      let mod_fields = Object.keys(this.personForm.controls)
+      for (let el of mod_fields) {
+        let fp = this.personForm.get(el)
+
+        //this.logControl(el, fp)
+        if (fp && fp.invalid && (fp.dirty || fp.touched)) {
+          errors[el] = fp.errors
+        }
+      }
+    }
+    //console.log(JSON.stringify(errors))
+    return errors
+  }
+
+  //-- END <!--Form Error Observable -->
+
+  private logControl(name, control) {
+    // if (name != 'lastname') return;
+    let s = `${name} - pristine:${control.pristine} - dirty:${control.dirty} - touched:${control.touched} - untouched:${control.untouched} - value:${control.value} - status:${control.status} - valid:${control.valid} - invalid:${control.invalid} - errors:${JSON.stringify(control.errors)}`
+    console.log(s)
+  }
+
 
   savePerson(personForm): void {
     this.ps.save(personForm);
@@ -113,8 +164,8 @@ export class ReactiveValidationComponent implements OnInit {
     if (control.value === 'Besenstiel') {
       return { nameError: true };
     }
-    if(control.value?.length<4 || control.value?.length>15) {
-      return { lengthError: true}
+    if (control.value?.length < 4 || control.value?.length > 15) {
+      return { lengthError: true }
     }
     return null;
   }
