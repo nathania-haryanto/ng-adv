f6b3e51d2277984c3cee0e1508571711dc391afb
Alexander Pajer <alexander.pajer@integrations.at>
2021-09-06 09:55:06 +0200

    ng12 upgrade

--- Demos/05-Routing-Security/02-FirebaseAuth/FireBaseAuth/src/environments/environment.ts
+++ Demos/05-Routing-Security/02-FirebaseAuth/FireBaseAuth/src/environments/environment.ts
@@ -3,21 +3,11 @@ export const environment = {
   authEnabled: false,
   title: 'ngSecurity',
   firebaseConfig: {
-    apiKey: "AIzaSyANuhe0585hdB8fdkiPedhJq0QMCuai6so",
-    authDomain: "angular-c6f6e.firebaseapp.com",
-    databaseURL: "https://angular-c6f6e.firebaseio.com",
-    projectId: "angular-c6f6e",
-    storageBucket: "angular-c6f6e.appspot.com",
-    messagingSenderId: "1065242827410",
-    appId: "1:1065242827410:web:0658b9d075e68032"
-  },
-  o365Config: {
-    tenant: 'bb3c3add-6082-4e1a-9773-4fef7e2f780e',
-    clientId: 'ac616495-0b28-49a3-8054-6a76a6afb90f',
-    cacheLocation: 'localStorage',
-    endpoints: {
-      graphApiUri: 'https://graph.microsoft.com',
-    },
-    returnUrl: 'http://localhost:4200',
+    apiKey: 'AIzaSyDK-xtrfT4YBzq4Hzmf5kEwHgHbYw4UQTc',
+    authDomain: 'ngrx-firebase-fb51b.firebaseapp.com',
+    projectId: 'ngrx-firebase-fb51b',
+    storageBucket: 'ngrx-firebase-fb51b.appspot.com',
+    messagingSenderId: '141384369616',
+    appId: '1:141384369616:web:659d331b4ad7a3f16365d5',
   },
 };
