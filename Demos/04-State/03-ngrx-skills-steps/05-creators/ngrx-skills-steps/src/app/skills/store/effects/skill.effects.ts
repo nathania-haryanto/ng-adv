d3c740bb65a4e18bf9b5d42d53da2390bb1785d8
Alexander Pajer <alexander.pajer@integrations.at>
2021-09-07 09:48:47 +0200

    skill effekt fix

--- Demos/04-State/03-ngrx-skills-steps/05-creators/ngrx-skills-steps/src/app/skills/store/effects/skill.effects.ts
+++ Demos/04-State/03-ngrx-skills-steps/05-creators/ngrx-skills-steps/src/app/skills/store/effects/skill.effects.ts
@@ -16,29 +16,29 @@ export class SkillsEffects {
   constructor(private actions$: Actions, private service: SkillsService) {}
 
   // Note: Make sure you are starting json-server or your api before using this
-  @Effect()
-  loadSkills$: Observable<Action> = this.actions$.pipe(
-    ofType(loadSkills),
-    mergeMap(() =>
-      this.service.getSkills().pipe(
-        map((skills: Skill[]) => loadSkillsSuccess({ skills })),
-        catchError((err) => of(loadSkillsError({ err })))
-      )
-    )
-  );
-
-  // loadSkills$ = createEffect(() =>
-  //   this.actions$.pipe(
-  //     ofType(loadSkills),
-  //     mergeMap(() =>
-  //       this.service.getSkills().pipe(
-  //         map((skills) => ({
-  //           type: '[Skill] Load Skills Success',
-  //           payload: skills,
-  //         })),
-  //         catchError((err) => of(loadSkillsError({ err })))
-  //       )
+  // @Effect()
+  // loadSkills$: Observable<Action> = this.actions$.pipe(
+  //   ofType(loadSkills),
+  //   mergeMap(() =>
+  //     this.service.getSkills().pipe(
+  //       map((skills: Skill[]) => loadSkillsSuccess({ skills })),
+  //       catchError((err) => of(loadSkillsError({ err })))
   //     )
   //   )
   // );
+
+  loadSkills$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadSkills),
+      mergeMap(() =>
+        this.service.getSkills().pipe(
+          map((skills) => ({
+            type: '[Skill] Load Skills Success',
+            skills: skills,
+          })),
+          catchError((err) => of(loadSkillsError({ err })))
+        )
+      )
+    )
+  );
 }
