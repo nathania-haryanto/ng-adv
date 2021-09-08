ec6f104b2fcd97cdd067216cdc7299d3996831ab
Alexander Pajer <alexander.pajer@integrations.at>
2021-09-08 07:49:33 +0200

    ng12 update elements

--- Demos/07-Reusability/03-Elements/skills-elements/src/app/app.module.ts
+++ Demos/07-Reusability/03-Elements/skills-elements/src/app/app.module.ts
@@ -2,14 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { SkillsListComponent } from './skills-list/skills-list.component';
+
+// @NgModule({
+//   declarations: [AppComponent, SkillsListComponent],
+//   imports: [BrowserModule, AppRoutingModule, FormsModule],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
+
+import { createCustomElement } from '@angular/elements';
 import { SkillsListComponent } from './skills-list/skills-list.component';
 
 @NgModule({
-  declarations: [AppComponent, SkillsListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [SkillsListComponent],
+  imports: [BrowserModule, FormsModule],
+  entryComponents: [SkillsListComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(): void {
+    const el = createCustomElement(SkillsListComponent, {
+      injector: this.injector,
+    });
+
+    customElements.define('ng-skills', el);
+  }
+}
--- Demos/07-Reusability/03-Elements/skills-elements/src/polyfills.ts
+++ Demos/07-Reusability/03-Elements/skills-elements/src/polyfills.ts
@@ -64,3 +64,4 @@ import 'zone.js'; // Included with Angular CLI.
  */
 import '@webcomponents/custom-elements/src/native-shim';
 import '@webcomponents/custom-elements/custom-elements.min';
+import 'document-register-element';
