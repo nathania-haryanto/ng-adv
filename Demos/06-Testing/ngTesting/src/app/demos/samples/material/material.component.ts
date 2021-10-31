9020ba91b57fe9e0ddf1cbdedbba304c234b5699
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-31 17:38:39 +0100

    mat testing harness

--- Demos/06-Testing/ngTesting/src/app/demos/samples/material/material.component.ts
+++ Demos/06-Testing/ngTesting/src/app/demos/samples/material/material.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-material',
   templateUrl: './material.component.html',
-  styleUrls: ['./material.component.scss']
+  styleUrls: ['./material.component.scss'],
 })
-export class MaterialComponent implements OnInit {
+export class MaterialComponent {
+  value = 50;
+  slidervalue;
+  validated = false;
+  sliderForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.sliderForm = new FormGroup({
+      slider: new FormControl(this.value, Validators.min(1)),
+    });
 
-  ngOnInit(): void {
+    this.sliderForm.valueChanges.subscribe(
+      (data: any) => (this.value = data.slider)
+    );
   }
 
+  resetSlider() {
+    this.sliderForm.controls['slider'].setValue(1);
+  }
 }
