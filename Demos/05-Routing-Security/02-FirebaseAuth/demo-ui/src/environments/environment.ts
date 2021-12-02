85dd870f747c0aaf04c2f9d124740f73d88093a7
Alexander Pajer <alexander.pajer@integrations.at>
2021-12-02 09:58:22 +0100

    temp

--- Demos/05-Routing-Security/02-FirebaseAuth/demo-ui/src/environments/environment.ts
+++ Demos/05-Routing-Security/02-FirebaseAuth/demo-ui/src/environments/environment.ts
@@ -4,6 +4,14 @@ export const environment = {
   title: 'ngSecurity',
   markdownPath: '/assets/markdown/',
   api: 'https://localhost:5001/',
-  firebaseConfig: {},
+  firebaseConfig: {
+    apiKey: 'AIzaSyA8dcddU1_6r7mpFvwBtnJ-CEvH0UE5D28',
+    authDomain: 'foodapp-63857.firebaseapp.com',
+    projectId: 'foodapp-63857',
+    storageBucket: 'foodapp-63857.appspot.com',
+    messagingSenderId: '230135302900',
+    appId: '1:230135302900:web:ae852b11ede38d110faca1',
+    measurementId: 'G-DH8NGC6175',
+  },
   o365Config: {},
 };
--- Demos/08-PWA/readme.md
+++ Demos/08-PWA/readme.md
@@ -14,7 +14,7 @@
 
 ### Prerequisites: Skills Api
 
-- Requires [.NET 5 SDK](https://dotnet.microsoft.com/download/dotnet/5.0).
+- Requires [.NET 6 SDK](https://dotnet.microsoft.com/download/dotnet/6.0).
 - If you want to use another db engine change connection string in appsettings.json.
 
 Go to folder where `*.csproj` is located, open console and run:
