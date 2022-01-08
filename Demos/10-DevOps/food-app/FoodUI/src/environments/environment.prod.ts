39db5448c8ab69fc266f7980d36d657e15db3026
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-08 19:12:32 +0100

    ui-with-configmap

--- Demos/10-DevOps/food-app/FoodUI/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  authEnabled: true,
-  apiurl: "https://localhost:5001/api/",
-  appInsights: {
-    instrumentationKey: "85035b0d-643a-4e46-91e0-43365c069088",
-  },
-};
--- Demos/10-DevOps/food-app/FoodUI/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  authEnabled: false,
-  apiurl: "https://localhost:5001/api/",
-  appInsights: {
-    instrumentationKey: "85035b0d-643a-4e46-91e0-43365c069088",
-  },
-};
