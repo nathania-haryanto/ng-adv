00ee9159ebbf509f427fdb7262a77deb7e9c1739
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-09 08:02:03 +0200

    effect bugs fixed

--- Demos/04-State/01-demo-app/src/app/demos/demo-base/demo.service.ts
+++ Demos/04-State/01-demo-app/src/app/demos/demo-base/demo.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { DemoItem } from './demo-item.model';
 import { environment } from '../../../environments/environment';
+import { DemoItem } from './demo-item.model';
 
 @Injectable({ providedIn: 'root' })
 export class DemoService {
   constructor(private httpClient: HttpClient) {}
 
-  getItems(): Observable<DemoItem[]> {
+  getDemos(): Observable<DemoItem[]> {
     return this.httpClient.get<DemoItem[]>(environment.demosApi);
+  }
+
+  deleteDemo(id: number): Observable<any> {
+    return this.httpClient.delete(`${environment.apiUrl}skills/${id}`);
   }
 }
