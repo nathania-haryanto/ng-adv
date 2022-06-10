ac479e7dc99595c9f4175fd91d97237e3bab0266
Alexander Pajer <alexander.pajer@integrations.at>
2022-06-10 20:06:53 +0200

    formbuilder

--- Demos/02-ReactiveForms/ngReactiveForms/src/app/demos/samples/forms-builder/forms-builder.component.ts
+++ Demos/02-ReactiveForms/ngReactiveForms/src/app/demos/samples/forms-builder/forms-builder.component.ts
@@ -1,40 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Person } from '../person/person.model';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Person, wealthOptsValues } from '../person/person.model';
 import { PersonService } from '../person/person.service';
+import { PersonFormType } from '../person.form';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-builder',
   templateUrl: './forms-builder.component.html',
   styleUrls: ['./forms-builder.component.scss'],
 })
-export class FormsBuilderComponent {
-  // personForm: FormGroup;
-  // person: Person = emptyPerson;
-  // wealthOpts = wealthOpts;
-  // constructor(private fb: FormBuilder, private ps: PersonService) {}
-  // ngOnInit() {
-  //   this.ps.getPerson().subscribe((p) => {
-  //     //Reminder: setValue vs patchValue
-  //     this.personForm.patchValue(p);
-  //     console.log('Data loaded from service', p);
-  //   });
-  //   this.personForm = this.fb.group({
-  //     id: [this.person.name],
-  //     name: [this.person.name, Validators.required],
-  //     lastname: [this.person.lastname, Validators.required],
-  //     age: [this.person.age],
-  //     gender: [this.person.gender],
-  //     email: [this.person.email],
-  //     wealth: [this.person.wealth],
-  //   });
-  //   setTimeout(() => {
-  //     //Use this to update form incrementally
-  //     this.personForm.patchValue({ name: 'Soi' });
-  //     console.log('Cleo changed to Soi');
-  //   }, 3000);
-  // }
-  // savePerson(personForm): void {
-  //   this.ps.save(personForm);
-  // }
+export class FormsBuilderComponent implements OnInit {
+  person: Person = new Person();
+  wealthOpts = wealthOptsValues;
+
+  personForm = new FormGroup<PersonFormType>({
+    id: new FormControl(0),
+    name: new FormControl(''),
+    age: new FormControl(0),
+    email: new FormControl(''),
+    gender: new FormControl('male'),
+    wealth: new FormControl(''),
+  });
+
+  constructor(private fb: FormBuilder, private ps: PersonService) {}
+
+  ngOnInit() {
+    this.ps.getPerson().subscribe((p) => {
+      //Reminder: setValue vs patchValue
+      this.personForm.patchValue(p);
+      console.log('Data loaded from service', p);
+    });
+
+    this.personForm = this.fb.group({
+      id: [this.person.id],
+      name: [this.person.name, Validators.required],
+      age: [this.person.age],
+      gender: [this.person.gender],
+      email: [this.person.email],
+      wealth: [this.person.wealth],
+    });
+
+    setTimeout(() => {
+      //Use this to update form incrementally
+      this.personForm.patchValue({ name: 'Soi' });
+      console.log('Cleo changed to Soi');
+    }, 3000);
+  }
+
+  toggleId() {
+    this.personForm.controls.id.disable();
+  }
+
+  savePerson(): void {
+    this.ps.save(this.personForm as unknown as NgForm);
+    console.log('Getting raw value of id:', this.personForm.getRawValue().id);
+  }
 }
--- Demos/02-ReactiveForms/ngReactiveForms/src/app/demos/samples/person.form.ts
+++ Demos/02-ReactiveForms/ngReactiveForms/src/app/demos/samples/person.form.ts
@@ -1,10 +1,11 @@
 import { FormControl, FormGroup } from '@angular/forms';
 
 //not necessary but makes the form type
-export const PersonFormType = FormGroup<{
-    name: FormControl<string | null>,
-    age: FormControl<number | null>,
-    email: FormControl<string | null>,
-    gender: FormControl<'male' | 'female' | 'not set' | null>,
-    wealth: FormControl<string | null>
-}>
+export interface PersonFormType {
+  id: FormControl<number | null>;
+  name: FormControl<string | null>;
+  age: FormControl<number | null>;
+  email: FormControl<string | null>;
+  gender: FormControl<'male' | 'female' | 'not set' | null>;
+  wealth: FormControl<string | null>;
+}
