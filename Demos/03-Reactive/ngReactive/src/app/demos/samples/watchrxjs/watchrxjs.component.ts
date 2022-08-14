20f6bb4ce88df4ea9fdb9467a12d6b029db1bb00
Alexander Pajer <alexander.pajer@integrations.at>
2022-08-14 10:21:19 +0200

    marble testing

--- Demos/03-Reactive/ngReactive/src/app/demos/samples/watchrxjs/watchrxjs.component.ts
+++ Demos/03-Reactive/ngReactive/src/app/demos/samples/watchrxjs/watchrxjs.component.ts
@@ -16,12 +16,12 @@ export class WatchRxJsComponent implements OnInit {
   ngOnInit() {}
 
   watchRxJS() {
-    interval(2000)
-      .pipe(
-        watch('Interval (2000)', 10),
-        filter((v) => v % 2 === 0),
-        watch('Filter odd numbers out', 10)
-      )
-      .subscribe();
+    // interval(2000)
+    //   .pipe(
+    //     watch('Interval (2000)', 10),
+    //     filter((v) => v % 2 === 0),
+    //     watch('Filter odd numbers out', 10)
+    //   )
+    //   .subscribe();
   }
 }
---
+++ Demos/03-Reactive/ngReactive/src/assets/markdown/marble-testing.md
@@ -0,0 +1,6 @@
+- Examine Marble Testing Code in `/demos/samples/marble-testing`:
+
+![marbles](assets/images/marbles.jpg)
+
+
+[Testing RxJS Code with Marble Diagrams](https://rxjs.dev/guide/testing/marble-testing)
