a5d46edced1f144360d5c9915fd45fcfde8d5302
Alexander Pajer <alexander.pajer@integrations.at>
2022-08-21 09:10:12 +0200

    scaffold

---
+++ Demos/04-State/tutorials/data/ngrx-data-simple/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    EntityDataModule.forRoot(entityConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Demos/04-State/tutorials/data/readme.md
@@ -0,0 +1,13 @@
+# NgRx Data Simple
+
+Create project:
+
+```bash
+ng new ngrx-data-simple
+```
+
+Add NgRx:
+
+```bash
+ng add @ngrx/data@latest
+```
