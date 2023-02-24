046fe712f7b6c77aeecca3eb6b5df1544002872c
Alexander Pajer <alexander.pajer@integrations.at>
2023-02-24 19:10:26 +0100

    img opt

--- demos/04-state/ngrx-state/src/app/shared/shared.module.ts
+++ demos/04-state/ngrx-state/src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -29,6 +29,7 @@ const comps = [
     RouterModule,
     MaterialModule,
     FlexLayoutModule,
+    NgOptimizedImage,
     MarkdownModule.forRoot({
       loader: HttpClient,
     }),
