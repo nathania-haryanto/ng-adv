89833759c928bf53f9ee760a0acab09dc23cc324
Alexander Pajer <alexander.pajer@integrations.at>
2023-02-26 18:55:52 +0100

    setup

--- setup/setup-angular-dev.ps1
+++ setup/setup-angular-dev.ps1
@@ -1,5 +1,5 @@
 # Install chocolatey
-Write-Host "Installing Chocolatey - 1/6" -ForegroundColor yellow
+Write-Host "Installing Chocolatey - 1/4" -ForegroundColor yellow
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
@@ -14,36 +14,36 @@ choco install curl -y
 choco install nvm -y
 
 # Install VS Code Extensions
-Write-Host "VS Code Extensions - 5/6" -ForegroundColor yellow
+Write-Host "VS Code Extensions - 2/4" -ForegroundColor yellow
 
 code --install-extension ms-dotnettools.csharp
 code --install-extension ms-vscode.azurecli
 code --install-extension ms-vscode.azure-account
-code --install-extension ms-azuretools.vscode-azureappservice
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension GitHub.vscode-pull-request-github
-code --install-extension redhat.vscode-yaml
 code --install-extension angular.ng-template
 code --install-extension 1tontech.angular-material
 code --install-extension mikael.angular-beastcode
-code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension mhutchie.git-graph 
 code --install-extension hbenl.vscode-test-explorer
 code --install-extension raagh.angular-karma-test-explorer
 code --install-extension xabikos.jasminesnippets
 
-
 # Refresh Path Env
-Write-Host "Refresh Path Env - 4/6" -ForegroundColor yellow
+Write-Host "Refresh Path Env - 3/4" -ForegroundColor yellow
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
-# Install Node & Angular
-Write-Host "Installing Angular - 6/6" -ForegroundColor yellow
+# Install NVM
+Write-Host "Installing Node using NVM, Angular & json-server - 4/4" -ForegroundColor yellow
 nvm install 16.15.0
 nvm use 16.15.0
 
+# Install Json-server
+npm install -g json-server
+
+# Install Node & Angular
 npx @angular/cli@latest analytics off
 npm i -g @angular/cli
 
