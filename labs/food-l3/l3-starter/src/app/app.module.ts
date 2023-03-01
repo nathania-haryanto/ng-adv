ee671121e587ff19cca858b332501d48708029c8
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-01 11:55:48 +0100

    l3-starter

--- labs/food-l3/l3-starter/src/app/app.module.ts
+++ labs/food-l3/l3-starter/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutComponent } from './about/about.component';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FoodModule } from './food/food.module';
-import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
-import { MaterialModule } from './material.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { FoodContainerComponent } from './food/food-container/food-container.component';
+import { FoodListComponent } from './food/food-list/food-list.component';
+import { FoodEditComponent } from './food/food-edit/food-edit.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,9 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     NavbarComponent,
     SidebarComponent,
     MainComponent,
+    FoodContainerComponent,
+    FoodListComponent,
+    FoodEditComponent,
     HomeComponent,
     AboutComponent,
   ],
@@ -31,7 +36,6 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FoodModule,
     FlexLayoutModule,
   ],
   providers: [],
--- labs/food-l3/l3-starter/src/app/food/food-edit/food-edit.component.ts
+++ labs/food-l3/l3-starter/src/app/food/food-edit/food-edit.component.ts
@@ -4,15 +4,15 @@ import {
   Input,
   Output,
   EventEmitter,
-  SimpleChanges
+  SimpleChanges,
 } from '@angular/core';
-import { FoodItem } from 'src/app/food/food.model';
+import { FoodItem } from 'src/app/food/foodItem';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-food-edit',
   templateUrl: './food-edit.component.html',
-  styleUrls: ['./food-edit.component.scss']
+  styleUrls: ['./food-edit.component.scss'],
 })
 export class FoodEditComponent implements OnInit {
   constructor(private fb: UntypedFormBuilder) {
@@ -20,7 +20,7 @@ export class FoodEditComponent implements OnInit {
       id: 0,
       name: ['', [Validators.required, Validators.minLength(3)]],
       price: [0, Validators.min(1)],
-      calories: 0
+      calories: 0,
     });
   }
 
