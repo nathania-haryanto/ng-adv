a4c1dcbda4a479db75b63880e7473306220f53aa
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-02 18:51:04 +0100

    testing completed

--- demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/ngrx-reducers/reducers/reducers.component.spec.ts
+++ demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/ngrx-reducers/reducers/reducers.component.spec.ts
@@ -2,22 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReducersComponent } from './reducers.component';
 
-describe('ReducersComponent', () => {
-  let component: ReducersComponent;
-  let fixture: ComponentFixture<ReducersComponent>;
+describe('DemoReducer', () => {
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ReducersComponent ]
-    })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(ReducersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
