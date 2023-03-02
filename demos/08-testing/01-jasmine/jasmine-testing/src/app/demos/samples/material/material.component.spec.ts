5973f1a7cea3a4e96f67c03f990e952943b6d51f
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-02 10:47:02 +0100

    material harness

--- demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/material/material.component.spec.ts
+++ demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/material/material.component.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MaterialComponent } from './material.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { HarnessLoader } from '@angular/cdk/testing';
+import { MaterialComponent } from './material.component';
+import { MatInputModule } from '@angular/material/input';
 // Notice manual import of ...Harness
-import { MatSliderHarness } from '@angular/material/slider/testing';
-import { MatButtonHarness } from '@angular/material/button/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatSliderHarness } from '@angular/material/slider/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
 
 describe('MaterialComponent', () => {
   let component: MaterialComponent;
@@ -22,7 +23,7 @@ describe('MaterialComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [MaterialComponent],
-      imports: [MatCardModule, ReactiveFormsModule, MatSliderModule, MatButtonModule, BrowserAnimationsModule],
+      imports: [MatCardModule, ReactiveFormsModule, MatInputModule, MatSliderModule, MatButtonModule, BrowserAnimationsModule],
     }).compileComponents();
     fixture = TestBed.createComponent(MaterialComponent);
     fixture.detectChanges();
@@ -38,5 +39,13 @@ describe('MaterialComponent', () => {
     await btnResetHarness.click();
     const thumb = await sliderHarness.getEndThumb();
     expect(await thumb.getValue()).toBe(1);
+  });
+
+  it('should have the correct value in the input', async () => {
+    const input = await loader.getHarness(MatInputHarness);
+    expect(await input.getValue()).toBe('Sushi');
+
+    await input.setValue('Pizza');
+    expect(await input.getValue()).toBe('Pizza');
   });
 });
