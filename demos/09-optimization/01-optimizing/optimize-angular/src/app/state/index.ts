39466ec5ae390b749a42935055d5630df107f8ce
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-08 20:15:06 +0100

    logging

--- demos/09-optimization/01-optimizing/optimize-angular/src/app/state/index.ts
+++ demos/09-optimization/01-optimizing/optimize-angular/src/app/state/index.ts
@@ -6,11 +6,9 @@ export interface State {
   app: AppState;
 }
 
-export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+export function logNgRX(reducer: ActionReducer<any>): ActionReducer<any> {
   return function (state, action) {
-    // var ai: ApplicationInsights = AILoggerService.getInstance();
     console.log('ngrx', action.type);
-
     return reducer(state, action);
   };
 }
@@ -19,6 +17,6 @@ export const reducers: ActionReducerMap<State> = {
   app: appReducer,
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? [debug]
+export const metaReducers: MetaReducer<State>[] = environment.logNgRx
+  ? [logNgRX]
   : [];
