18f86af96419fd56b0158c7f090c7d64daeb0a02
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-08 19:54:08 +0100

    skills added to api

--- demos/09-optimization/01-optimizing/optimize-angular/src/app/state/index.ts
+++ demos/09-optimization/01-optimizing/optimize-angular/src/app/state/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { appReducer, AppState } from './app.reducer';
 
@@ -6,10 +6,19 @@ export interface State {
   app: AppState;
 }
 
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    // var ai: ApplicationInsights = AILoggerService.getInstance();
+    console.log('ngrx', action.type);
+
+    return reducer(state, action);
+  };
+}
+
 export const reducers: ActionReducerMap<State> = {
   app: appReducer,
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
+  ? [debug]
   : [];
